ac0a44168bc22be1aac0ee6c41455cdcf4381392
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-09-06 08:30:44 +0300

    start

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,13 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  // Arrange
   render(<App />);
+
+  // Act
+  // nothing
+
+  // Assert (Expect)
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
