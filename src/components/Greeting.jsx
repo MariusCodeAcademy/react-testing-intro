f3902733d407cb35be845017f52088be5dd007c7
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-09-06 09:01:06 +0300

    button

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,9 +1,20 @@
+import { useState } from 'react';
+
 export default function Greeting() {
+  const [changedText, setChangedText] = useState(false);
+
+  function buttonHandler() {
+    setChangedText(true);
+  }
+
   return (
     <div>
       <h2>Hello world!</h2>
 
-      <p>We are learnig Testing React</p>
+      {!changedText && <p>We are learnig Testing React</p>}
+      {changedText && <p>Changed!</p>}
+
+      <button onClick={buttonHandler}>Click me</button>
     </div>
   );
 }
